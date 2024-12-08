467d23c6051a64716a2cc584f7c0f56f8b85ad57
Sumra Jabeen <160333925+Coderhubs@users.noreply.github.com>
2024-12-08 20:56:11 +0500

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Header from "./components/Header";
 import Ads from "./components/Ads";
 import Booking from "./components/Booking";
-import { Footer } from "./components/footer";
 import PopularCard from "./components/PopularCard";
 import RecommendedCar from "./components/RecommendedCar";
-// import Catalogue from "./components/Catalogue";
+import { Footer } from "./components/Footer";
+
 
 export default function Home() {
   return (
